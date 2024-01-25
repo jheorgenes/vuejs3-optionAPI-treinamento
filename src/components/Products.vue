b59c104bd5497e3dfb8d72d2f333ba14e72069a0
Jheorgenes Warlley <jheorgenes@gmail.com>
2024-01-25 11:55:12 -0300

    - Aprendendo a utilizar Scoped Slot, que diferentemente do slot comum, realiza uma interação mais profunda nos recursos do Vue de interação entre componentes.

---
+++ src/components/Products.vue
@@ -0,0 +1,13 @@
+<template>
+<ul>
+  <!-- Aqui está percorrendo uma lista de produtos e inserindo a tag slot para cada produto da lista -->
+  <!-- Dentro de cada item da lista percorrido, está sendo ENVIADO o conteúdo do item com o nome de 'product' -->
+  <slot v-for="(product, index) in products" :key="index" :product="product"></slot>
+</ul>
+</template>
+
+<script>
+export default {
+  props: ['products']
+}
+</script>
--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,27 +1,48 @@
 <template>
-  <h2 id="app-products">Products</h2>
-  <h2 id="my-app">Products my app</h2>
-
-  <h2>Product 1</h2>
-  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea aut animi voluptate in rem odit officiis hic culpa deleniti commodi, veritatis assumenda nulla, vero enim reiciendis asperiores autem harum laboriosam.</p>
-  <ProductInfo :likes="product.likes" :comments="product.comments" />
-
+<!-- Abaixo, está sendo chamado o componente Products, enviando a propriedade 'products' desse View para o componente e obtendo (através do v-slot="props") o acesso as propriedades do componente 'Products' de forma indireta -->
+<!-- No corpo do componente, estamos enviando um conteúdo que será exibido dentro da tag slot -->
+<!-- Mas esse conteúdo que estamos enviando para o slot, foi obtido indiretamente do componente 'Products' através do (v-slot="props") -->
+<!-- O conteúdo que está sendo obtido dessa (v-slot="props"), é resultado de uma iteração no componente, e é colocada aqui nesse View, dentro da estrutura de uma tag li, para então enviá-la dentro do slot anteriormente definido. -->
+<Products :products="products" v-slot="props">
+  <li @click="getProdutct(props.product.id)">{{ props.product.name }}</li>
+</Products>
 </template>
 
 <script>
 import ProductInfo from '@/components/ProductInfo.vue';
+import Products from '@/components/Products.vue';
 
 export default {
   components: {
-    ProductInfo
+    ProductInfo,
+    Products
   },
   name: 'Products.vue',
   data: () => ({
+    products: [
+      {
+        id: 1,
+        name: 'Imac'
+      },
+      {
+        id: 2,
+        name: 'MacBook'
+      },
+      {
+        id: 3,
+        name: 'Mouse'
+      },
+    ],
     product: {
       likes: 20,
       comments: 30
     }
   }),
+  methods: {
+    getProdutct(id) {
+      console.log(id);
+    }
+  }
 }
 </script>
 
